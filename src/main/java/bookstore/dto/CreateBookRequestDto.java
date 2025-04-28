a81531151c0a2fab68257d749439c107afd7fd3e
Karol Gajda <karolgaj97@interia.pl>
2025-04-28 15:43:24 +0200

    Adding CustomGlobalExceptionHandler, Adding Validation To createBookRequestDTO, Making UniqueField Validation

--- src/main/java/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/bookstore/dto/CreateBookRequestDto.java
@@ -1,5 +1,8 @@
 package bookstore.dto;
 
+import bookstore.validation.UniqueField;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +13,17 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class CreateBookRequestDto {
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @UniqueField
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
+    @NotNull
     private String coverImage;
 }
