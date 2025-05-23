d1f0734911801f87d418e224ef5d6a9b4ec12797
Karol Gajda <karolgaj97@interia.pl>
2025-05-23 15:33:08 +0200

    Adding Category to books , Category controler, repository, mapper, service

--- src/main/java/bookstore/mapper/BookMapper.java
+++ src/main/java/bookstore/mapper/BookMapper.java
@@ -2,20 +2,37 @@ package bookstore.mapper;
 
 import bookstore.config.MapperConfig;
 import bookstore.dto.book.BookDto;
+import bookstore.dto.book.BookDtoWithoutCategoryIds;
 import bookstore.dto.book.CreateBookRequestDto;
 import bookstore.model.Book;
+import bookstore.model.Category;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
 
+    @Mapping(source = "category", target = "category")
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto createBookRequestDto);
 
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateModelFromDto(CreateBookRequestDto dto, @MappingTarget Book entity);
+
+    default String map(Set<Category> categories) {
+        if (categories == null || categories.isEmpty()) {
+            return "";
+        }
+        return categories.stream()
+                .map(Category::getName)
+                .collect(Collectors.joining(", "));
+    }
 }
--- src/main/java/bookstore/repository/book/BookRepository.java
+++ src/main/java/bookstore/repository/book/BookRepository.java
@@ -1,11 +1,18 @@
 package bookstore.repository.book;
 
 import bookstore.model.Book;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+
     boolean existsByIsbn(String isbn);
+
+    @Query("SELECT b FROM Book b LEFT JOIN FETCH b.category c WHERE c.id = :categoryId")
+    List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
 }
